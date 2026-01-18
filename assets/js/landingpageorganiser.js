f2828c38d9bc8e0db2e33c45ce1d07a1799b918c
Ephyjack <ephraimtech390@gmail.com>
2026-01-18 22:52:12 +0100

    made changes

--- assets/js/landingpageorganiser.js
+++ assets/js/landingpageorganiser.js
@@ -26,7 +26,7 @@ nextBtn.addEventListener('click', () => {
     current++;
     updateSlide();
   } else {
-    window.location.href = '/attendeeconnect/organiser-app/auth/signup&login.html';
+    window.location.href = '/organiser-app/auth/signup&login.html';
   }
 });
 
