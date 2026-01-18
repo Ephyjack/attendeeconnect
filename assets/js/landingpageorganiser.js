3d025c4d337159fd8b7da102de2ff97a0c4d7f22
Ephyjack <ephraimtech390@gmail.com>
2026-01-18 23:19:57 +0100

    made changes

--- assets/js/landingpageorganiser.js
+++ assets/js/landingpageorganiser.js
@@ -26,7 +26,7 @@ nextBtn.addEventListener('click', () => {
     current++;
     updateSlide();
   } else {
-    window.location.href = '/auth/signup&login.html';
+    window.location.href = '/organiser/auth/signup&login.html';
   }
 });
 
@@ -38,7 +38,7 @@ backBtn.addEventListener('click', () => {
 });
 
 skipBtn.addEventListener('click', () => {
-  window.location.href = '/auth/signup&login.html';
+  window.location.href = '/organiser/auth/signup&login.html';
 });
 
 // Initialize
